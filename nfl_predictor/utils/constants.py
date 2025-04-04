291c304c2b95ec3e373140a0544540bbfad6b78c
William Hansen <soccerbh9@gmail.com>
2025-04-04 00:05:15 -0400

    Add agent and continue enhancements (#15)
    * Add upsets agent and make minor changes
    
    * Update return and variable naming
    
    * Resolve latest trap

---
+++ nfl_predictor/utils/constants.py
@@ -0,0 +1,13 @@
+# Base filenames
+INPUT_FILE_NAME = "upcoming_matchups_test.csv"
+INJURIES_FILE_NAME = "nfl_injuries_test.csv"
+PROPERTIES_FILE_NAME = "nfl_properties_test.yaml"
+STANDINGS_FILE_NAME = "standings_test.csv"
+OUTPUT_FILE_NAME = "predicted_matchups_test.csv"
+FLAGGED_OUTPUT_FILE_NAME = "predicted_matchups_flagged_test.csv"
+
+# Dynamic file templates for stats
+CONVERSIONS_FILE = "nfl_conversions_thru_week_{week}_{year}.csv"
+OFFENSE_FILE = "nfl_team_offense_thru_week_{week}_{year}.csv"
+DEFENSE_FILE = "nfl_team_defense_thru_week_{week}_{year}.csv"
+CONV_AGAINST_FILE = "nfl_conversions_against_thru_week_{week}_{year}.csv"
