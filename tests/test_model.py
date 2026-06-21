fa03fe07550074868f81c34324267162e78cd9e2
Bill Hansen <soccerbh9@gmail.com>
2026-06-20 22:04:47 -0400

    Fix week one scenario

--- tests/test_model.py
+++ tests/test_model.py
@@ -7,7 +7,7 @@ from nfl_predictor.utils.helpers import get_training_week, resolve_weeks
 @pytest.mark.parametrize(
     "week_value, expected",
     [
-        (1, (1, 1)),
+        (1, (1, 18)),
         (2, (2, 1)),
         ("3", (3, 2)),
         (np.int64(4), (4, 3)),
@@ -21,7 +21,7 @@ def test_resolve_weeks_handles_numeric_and_postseason_values(week_value, expecte
 @pytest.mark.parametrize(
     "week_value, expected",
     [
-        (0, 1),
+        (0, 18),
         (1, 1),
         (2, 2),
         ("3", 3),
